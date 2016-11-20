518fbf1667470c36dfadab4f8b8447c950cffdb1
= <ryan.murphy@takor.com.au>
2016-11-20 14:29:05 +0800

    Add a Static Pages controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def placeholder
-  	render html: "HEROKU PLACEHOLDER"
+  def hello
+  	render html: "hello world"
   end
 end
